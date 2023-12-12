d5cc00d2fd1c8a03637ae2521f3ae23584094dc7
Lorenzo Rossi <152617981+Lor3nzoRossi@users.noreply.github.com>
2023-12-12 10:20:42 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
-    
+    //commento random
   Matrice m = new Matrice ("TPSIT");
   Vigenere v = new Vigenere(0,12,0,12,m);
   Thread t = new Thread(v);
